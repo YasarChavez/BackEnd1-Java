9a4e3c2a24c58e2e88e7f7fa073f645061a2b2ab
Administrador <tneniitoo@gmail.com>
2023-02-22 07:57:27 -0300

    Modificaciones a los archivos.

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,4 +1,4 @@
-public class Funciones {
+public abstract class Funciones {
     public static void main(String[] args) {
     holaMundo();
     }
@@ -6,4 +6,13 @@ public class Funciones {
     public static void holaMundo() {
         System.out.println("Hola mundo!");
     }
+
+   public static void mostrarPI(){
+       double var1 = 355;
+       double var2 = 113;
+       System.out.println(var1/var2);
+   }
+
+
+
 }
