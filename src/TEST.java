2d78544d183c63490e6ae9b21b54b51caef7e715
Yasar <tneniitoo@gmail.com>
2023-02-21 22:28:44 -0300

    Modificaciones a los archivos.

---
+++ src/TEST.java
@@ -0,0 +1,18 @@
+public class TEST {
+    public static void main(String[] args) {
+        int num1 = 2;
+        //Funciones.holaMundo();
+        //System.out.println(num1);
+
+        for (int i = 0; i < 5; i++) {
+            Funciones.holaMundo();
+        }
+
+
+
+
+
+
+
+    }
+}
