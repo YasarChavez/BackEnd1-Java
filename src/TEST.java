32037498231680148ba57f6ebf347f2bf4d8ab12
Administrador <tneniitoo@gmail.com>
2023-02-23 20:50:15 -0300

    Modificaciones a los archivos.

--- src/TEST.java
+++ src/TEST.java
@@ -8,30 +8,37 @@ public class TEST {
     }
 
     public static void main(String[] args) {
-        System.out.println("Sumar 3+2");
-        Funciones.sumar(3.0, 2.0);
-        System.out.println("Restar 10-7");
-        Funciones.restar(10.0, 7.0);
-        System.out.println("Multiplicar 3*4");
-        Funciones.multiplicar(3.0, 4.0);
-        System.out.println("Dividir 10/3");
-        Funciones.dividir(10.0, 3.0);
-        System.out.println("Restar 100 - 50");
-        Funciones.restar(100,50);
-
-        System.out.println("Operaciones terminadas.");
-
-        int, long, double, boolean, String.
-        int n1=5;
-        System.out.println(n1);
-        long n2=5666;
-        System.out.println(n2);
-        double n3=50.256;
-        System.out.println(n3);
-        boolean verd = true;
-        System.out.println(verd);
-        String saludo = "Hola chicos!";
-        System.out.println(saludo);
+//        System.out.println("Sumar 3+2");
+//        Funciones.sumar(3.0, 2.0);
+//        System.out.println("Restar 10-7");
+//        Funciones.restar(10.0, 7.0);
+//        System.out.println("Multiplicar 3*4");
+//        Funciones.multiplicar(3.0, 4.0);
+//        System.out.println("Dividir 10/3");
+//        Funciones.dividir(10.0, 3.0);
+//        System.out.println("Restar 100 - 50");
+//        Funciones.restar(100,50);
+//
+//        System.out.println("Operaciones terminadas.");
+//
+//        int, long, double, boolean, String.
+//        int n1=5;
+//        System.out.println(n1);
+//        long n2=5666;
+//        System.out.println(n2);
+//        double n3=50.256;
+//        System.out.println(n3);
+//        boolean verd = true;
+//        System.out.println(verd);
+//        String saludo = "Hola chicos!";
+//        System.out.println(saludo);
+        final double PI = 3.1415926536;
+        double radio = 25.0, perimetro;
+        perimetro = 2.0*PI*radio;
+        System.out.print("El perimetro de la circunferencia de radio ");
+        System.out.println(radio);
+        System.out.print("es ");
+        System.out.print(perimetro);
 
 
     }
