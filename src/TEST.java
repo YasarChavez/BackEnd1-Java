22cbb1f534ea1614ed6fd37a8bed3c9364743438
Administrador <tneniitoo@gmail.com>
2023-02-23 08:51:41 -0300

    Modificaciones a los archivos.

--- src/TEST.java
+++ src/TEST.java
@@ -8,9 +8,15 @@ public class TEST {
     }
 
     public static void main(String[] args) {
+        System.out.println("Sumar 3+2");
         Funciones.sumar(3.0, 2.0);
+        System.out.println("Restar 10-7");
         Funciones.restar(10.0, 7.0);
+        System.out.println("Multiplicar 3*4");
         Funciones.multiplicar(3.0, 4.0);
+        System.out.println("Dividir 10/3");
         Funciones.dividir(10.0, 3.0);
+
+        System.out.println("Operaciones terminadas.");
     }
 }
