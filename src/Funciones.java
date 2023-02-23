769ae16ee64ffa7e957aa8346a072282ad69fa57
Administrador <tneniitoo@gmail.com>
2023-02-23 08:17:01 -0300

    Modificaciones a los archivos.

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,9 +1,25 @@
-public class Funciones {
-    public static void main(String[] args) {
-    holaMundo();
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public abstract class Funciones {
+    public Funciones() {
     }
 
-    public static void holaMundo() {
-        System.out.println("Hola mundo!");
+    public static void sumar(double n1, double n2) {
+        System.out.println(n1 + n2);
+    }
+
+    public static void restar(double n1, double n2) {
+        System.out.println(n1 - n2);
+    }
+
+    public static void multiplicar(double n1, double n2) {
+        System.out.println(n1 * n2);
+    }
+
+    public static void dividir(double n1, double n2) {
+        System.out.println(n1 / n2);
     }
 }
--- src/TEST.java
+++ src/TEST.java
@@ -1,18 +1,16 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 public class TEST {
+    public TEST() {
+    }
+
     public static void main(String[] args) {
-        int num1 = 2;
-        //Funciones.holaMundo();
-        //System.out.println(num1);
-
-        for (int i = 0; i < 5; i++) {
-            Funciones.holaMundo();
-        }
-
-
-
-
-
-
-
+        Funciones.sumar(3.0, 2.0);
+        Funciones.restar(10.0, 7.0);
+        Funciones.multiplicar(3.0, 4.0);
+        Funciones.dividir(10.0, 3.0);
     }
 }
