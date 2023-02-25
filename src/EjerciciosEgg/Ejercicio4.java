e89ea0bc091f8b7380263ce7e7927f777b527130
YasarChavez <tneniitoo@gmail.com>
2023-02-25 15:50:40 -0300

    Modificaciones a los archivos.

---
+++ src/EjerciciosEgg/Ejercicio4.java
@@ -0,0 +1,19 @@
+package EjerciciosEgg;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        //Define una variable que aloje tu nombre y otra que guarde tu edad. Imprime ambas variables por
+        //pantalla
+        Scanner leer = new Scanner(System.in);
+
+
+        System.out.println("Ingresa tu nombre:");
+        String nombre = leer.next(); //Definimos nombre y decimos que va a ser el valor leido con "scanner leer".
+        System.out.println("Ingresa tu edad:");
+        int edad = Integer.parseInt(leer.next());//Lo mismo para la edad "Integer.parseInt" guarda el valor como entero en lugar de texto.
+        System.out.println("Bienvenido " + nombre + ", veo que tienes " + edad + " años.");
+
+    }
+}
---
+++ src/EjerciciosEgg/Ejercicio5.java
@@ -0,0 +1,25 @@
+package EjerciciosEgg;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+    //Define una variable de tipo boolean, double y char. Guarda información en ellas a través del Scanner.
+        Scanner leer = new Scanner(System.in);
+        boolean bandera;
+        double numero;
+        String letra;
+        System.out.println("Ingrese un valor tipo boolean:");
+        bandera = Boolean.parseBoolean(leer.next());
+        System.out.println("Ingrese un valor tipo double:");
+        numero = Double.parseDouble(leer.next());
+        System.out.println("Ingrese un valor tipo char:");
+        letra = leer.next();
+        System.out.println(bandera);
+        System.out.println(numero);
+        System.out.println(letra);
+
+
+
+    }
+}
