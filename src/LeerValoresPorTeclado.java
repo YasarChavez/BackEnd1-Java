4d4e524546770f986b02f1fe5f32d8ca3c7158c0
YasarChavez <tneniitoo@gmail.com>
2023-02-25 08:29:48 -0300

    Modificaciones a los archivos.

--- src/LeerValoresPorTeclado.java
+++ src/LeerValoresPorTeclado.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class LeerValoresPorTeclado {
     public static void main(String[] args) {
         System.out.println("Bienvenido a tu primer ingreso de datos por teclado!");
-        Scanner leer = new Scanner(System.in);
-        String nombre;
+        Scanner leer = new Scanner(System.in); //Creamos variable Scanner para leer datos.
+        String nombre; //Definimos variable "nombre"
         System.out.println("Ingresa tu nombre:");
-        nombre = leer.next();
-        System.out.println("Bienvenido " + nombre + "!");
+        nombre = leer.next(); //Leemos "nombre"
+        System.out.println("Bienvenido " + nombre + "!"); //Escribimos "Texto" + nombre + "Texto"
         System.out.println("Espero que aprendas mucho el dia de hoy.");
     }
 }
