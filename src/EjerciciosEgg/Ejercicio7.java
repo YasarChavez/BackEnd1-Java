11a68f8e4f77d678638c28cabdb75c64e0ae5b4e
Yasar <tneniitoo@gmail.com>
2023-02-25 23:14:04 -0300

    Modificaciones a los archivos.

--- src/EjerciciosEgg/Ejercicio7.java
+++ src/EjerciciosEgg/Ejercicio7.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Ejercicio7 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        //Considera que estás desarrollando una web para una empresa que fabrica motores (suponemos
-        //que se trata del tipo de motor de una bomba para mover fluidos). Definir una variable tipoMotor y
-        //permitir que el usuario ingrese un valor entre 1 y 4. El programa debe mostrar lo siguiente:
+        /*Considera que estás desarrollando una web para una empresa que fabrica motores (suponemos
+        que se trata del tipo de motor de una bomba para mover fluidos). Definir una variable tipoMotor y
+        permitir que el usuario ingrese un valor entre 1 y 4. El programa debe mostrar lo siguiente:
 
-        //Si el tipo de motor es 1, mostrar un mensaje indicando “La bomba es una bomba de agua”.
-        //o Si el tipo de motor es 2, mostrar un mensaje indicando “La bomba es una bomba de
-        //gasolina”.
-        //o Si el tipo de motor es 3, mostrar un mensaje indicando “La bomba es una bomba de
-        //hormigón”.
-        //o Si el tipo de motor es 4, mostrar un mensaje indicando “La bomba es una bomba de pasta
-        //alimenticia”.
-        //o Si no se cumple ninguno de los valores anteriores mostrar el mensaje “No existe un valor
-        //válido para tipo de bomba”
+        Si el tipo de motor es 1, mostrar un mensaje indicando “La bomba es una bomba de agua”.
+        o Si el tipo de motor es 2, mostrar un mensaje indicando “La bomba es una bomba de
+        gasolina”.
+        o Si el tipo de motor es 3, mostrar un mensaje indicando “La bomba es una bomba de
+        hormigón”.
+        o Si el tipo de motor es 4, mostrar un mensaje indicando “La bomba es una bomba de pasta
+        alimenticia”.
+        o Si no se cumple ninguno de los valores anteriores mostrar el mensaje “No existe un valor
+        válido para tipo de bomba”*/
 
         System.out.println("Ingrese el numero del tipo de motor para recibir informacion detallada: (1 a 4)");
         int motor = leer.nextInt();
