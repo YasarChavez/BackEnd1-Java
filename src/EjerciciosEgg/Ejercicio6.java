648c68ccb72c11c3b8d5dbae7f8c9c22d87eba30
Yasar <tneniitoo@gmail.com>
2023-02-25 23:10:57 -0300

    Modificaciones a los archivos.

--- src/EjerciciosEgg/Ejercicio6.java
+++ src/EjerciciosEgg/Ejercicio6.java
@@ -24,39 +24,5 @@ public class Ejercicio6 {
             System.out.println("Ambos numeros son menores que 26!!!");
         }
 
-        int calificacion;
-
-        System.out.println("Ingresá la calificación que le das a la ultima pelicula que viste: (del 1 al 5)");
-        calificacion = leer.nextInt();
-
-        switch (calificacion) {
-            case 1:
-                System.out.println("Fue tan mala?");
-                break;
-            case 2:
-                System.out.println("Pudo ser peor no?");
-                break;
-            case 3:
-                System.out.println("Bueno, digamos que fue meeh xD");
-                break;
-            case 4:
-                System.out.println("Entonces estaba para pasar el rato almenos");
-                break;
-            case 5:
-                System.out.println("Que bueno que la disfrutaras tanto!");
-                break;
-            default:
-                System.out.println("Calificacion incorrecta, hasta pronto.");
-        }
-        /* OTRO TIPO DE SWITCH
-        switch (calificacion) {
-            case 1 -> System.out.println("Fue tan mala?");
-            case 2 -> System.out.println("Pudo ser peor no?");
-            case 3 -> System.out.println("Bueno, digamos que fue meeh xD");
-            case 4 -> System.out.println("Entonces estaba para pasar el rato almenos");
-            case 5 -> System.out.println("Que bueno que la disfrutaras tanto!");
-            default -> System.out.println("Calificacion incorrecta, hasta pronto.");
-        }*/
-
     }
 }
