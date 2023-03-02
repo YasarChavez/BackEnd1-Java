1667179070aa31123b59b89913a1d249e15090f5
YasarChavez <tneniitoo@gmail.com>
2023-03-02 08:35:24 -0300

    Modificaciones a los archivos.

--- src/Varios/TEST.java
+++ src/Varios/TEST.java
@@ -3,6 +3,7 @@ package Varios;//
 // (powered by FernFlower decompiler)
 //
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class TEST {
@@ -10,21 +11,38 @@ public class TEST {
     }
 
     public static void main(String[] args) {
-        for (int i = 1; i > 0; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println(i + " Fizz Buzz");
-            } else if (i % 5 == 0) {
-                System.out.println(i + " Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println(i + " Fizz");
-            } else System.out.println(i);
-            try {
-                TimeUnit.SECONDS.sleep(1);
-            } catch (Exception ignored) {
-            }
+//        for (int i = 1; i > 0; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println(i + " Fizz Buzz");
+//            } else if (i % 5 == 0) {
+//                System.out.println(i + " Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println(i + " Fizz");
+//            } else System.out.println(i);
+//            try {
+//                TimeUnit.SECONDS.sleep(1);
+//            } catch (Exception ignored) {
+//            }
+//
+//        }
 
+//        FOR
+//        Scanner leer = new Scanner(System.in);
+//        int matriz[][] = new int[3][3];
+//        for (int x=0; x < matriz.length; x++) {
+//            for (int y=0; y < matriz[x].length; y++) {
+//                (matriz[x][y])= (int) ((Math.random() * 9) + 1);
+//            }
+//        }
+//        for (int x=0; x < matriz.length; x++) {
+//            System.out.println("");
+//            for (int y=0; y < matriz[x].length; y++) {
+//                System.out.print(matriz[x][y]);
+//            }
+//        }
+        for (int i=0; i <=10; i++){
+            System.out.println("Numero " + i);
         }
-
     }
 
 }
