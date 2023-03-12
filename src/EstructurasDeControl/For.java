0ba374505d624abf9dd673045f04d47579bb32a9
YasarChavez <tneniitoo@gmail.com>
2023-03-12 12:12:00 -0300

    Creando ejemplos de estructuras de control.

--- src/EstructurasDeControl/For.java
+++ src/EstructurasDeControl/For.java
@@ -4,8 +4,11 @@ public class For {
     public static void main(String[] args) {
         //Escribir "Fizz" cuando I sea multiplo de 3.
         //"Buzz" cuando sea multiplo de 5 y "FizzBuzz" cuando es multiplo de ambos.
+        //Contar multiplos de 3, multiplos de 5 y multiplos de ambos.
 
-        for (int i = 1; i <=100; i++){
+        System.out.println("A buscar multiplos!");
+
+        for (int i = 1; i <=20; i++){
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i % 5 == 0) {
@@ -14,5 +17,6 @@ public class For {
                 System.out.println("Fizz");
             } else System.out.println(i);
         }
+        System.out.println("Programa terminado...");
     }
 }
