7451a5f518fcb0bd3374baa3f8eb25725b6b8e07
YasarChavez <tneniitoo@gmail.com>
2023-03-12 02:03:31 -0300

    Creando ejemplos de estructuras de control.

---
+++ src/EstructurasDeControl/IfElse.java
@@ -0,0 +1,27 @@
+package EstructurasDeControl;
+
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+        /*Crear un algoritmo que lea un numero y diga si es par o impar y tambien si es o no multiplo de 5*/
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese un numero:");
+        int num = leer.nextInt();
+
+        ///Vemos si es Par o Impar
+        if (num % 2 == 0) {
+            System.out.println("Es par!");
+        } else if (num % 2 != 0) {
+            System.out.println("Es impar!");
+        }
+
+        ///Vemos si es o no multiplo de 5
+        if (num % 5 == 0) {
+            System.out.println("Es multiplo de 5");
+        } else if (num % 5 != 0) {
+            System.out.println("No es multiplo de 5");
+        }
+
+    }
+}
