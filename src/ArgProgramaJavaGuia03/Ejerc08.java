bb042993bb6185a193af850e5e93913c08940ac2
YasarChavez <tneniitoo@gmail.com>
2023-03-13 18:54:51 -0300

    Ejercicios Guia 3

---
+++ src/ArgProgramaJavaGuia03/Ejerc08.java
@@ -0,0 +1,29 @@
+package ArgProgramaJavaGuia03;
+
+
+import java.util.Scanner;
+
+public class Ejerc08 {
+    public static void main(String[] args) {
+//Dibujar un cuadrado de N elementos por lado utilizando el carácter “*”. Por ejemplo, si el
+//cuadrado tiene 4 elementos por lado se deberá dibujar lo siguiente:
+//        * * * *
+//        *     *
+//        *     *
+//        * * * *
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el tamaño del cuadrado:");
+        int num = leer.nextInt();
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if (i == 0 || j == 0 || j == num - 1 || i == num - 1) {
+                    System.out.print(" *");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
