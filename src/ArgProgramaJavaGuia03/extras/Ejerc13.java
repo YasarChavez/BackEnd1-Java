bb89ea5dc1e74a7530f2aa1e41d9394b62cf7772
YasarChavez <tneniitoo@gmail.com>
2023-03-14 21:28:12 -0300

    Ejercicios Guia 3 extras.

---
+++ src/ArgProgramaJavaGuia03/extras/Ejerc13.java
@@ -0,0 +1,24 @@
+package ArgProgramaJavaGuia03.extras;
+
+import javax.swing.*;
+
+public class Ejerc13 {
+    public static void main(String[] args) {
+/*Crear un programa que dibuje una escalera de números, donde cada línea de números
+comience en uno y termine en el número de la línea.
+Solicitar la altura de la escalera al usuario al comenzar. Ejemplo: si se ingresa el número 3:
+1
+12
+123
+*/
+
+        int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el tamaño de la escalera"));
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < i ; j++){
+                System.out.print(j+1);
+            }System.out.println();
+        }
+
+    }
+}
