4e3490a31735386f23dacb2c2c3dde8deda981a6
YasarChavez <tneniitoo@gmail.com>
2023-03-20 18:38:06 -0300

    Ejercicios Guia 5

--- src/ArgentinaProgramaEGG/ArgProgramaJavaGuia05VectoresMatrices/Ejerc04.java
+++ src/ArgentinaProgramaEGG/ArgProgramaJavaGuia05VectoresMatrices/Ejerc04.java
@@ -7,7 +7,6 @@ y muestre la traspuesta de la matriz. La matriz traspuesta de una matriz
 A se denota por B y se obtiene cambiando sus filas por columnas (o
 viceversa).*/
         int [][] matriz1 = new int[4][4];
-        int [][] matriz2 = new int[4][4];
         //Llenamos matriz1
         for (int i = 0;i<4;i++){
             for (int j=0;j<4;j++){
