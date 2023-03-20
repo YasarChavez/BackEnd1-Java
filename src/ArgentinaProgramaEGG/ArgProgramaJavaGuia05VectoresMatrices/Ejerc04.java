e0b32487d102c63e20a162ac8c67faa411d06cc2
YasarChavez <tneniitoo@gmail.com>
2023-03-20 14:41:59 -0300

    Ejercicios Guia 5

---
+++ src/ArgentinaProgramaEGG/ArgProgramaJavaGuia05VectoresMatrices/Ejerc04.java
@@ -0,0 +1,34 @@
+package ArgentinaProgramaEGG.ArgProgramaJavaGuia05VectoresMatrices;
+
+public class Ejerc04 {
+    public static void main(String[] args) {
+        /*Realizar un programa que rellene un matriz de 4 x 4 de valores aleatorios
+y muestre la traspuesta de la matriz. La matriz traspuesta de una matriz
+A se denota por B y se obtiene cambiando sus filas por columnas (o
+viceversa).*/
+        int [][] matriz1 = new int[4][4];
+        int [][] matriz2 = new int[4][4];
+        //Llenamos matriz1
+        for (int i = 0;i<4;i++){
+            for (int j=0;j<4;j++){
+                matriz1[i][j]=(int) (Math.random()*(10-1)) + 1;
+            }
+        }
+        //Mostramos matriz1
+        System.out.println("Matriz 1");
+        for (int i = 0;i<4;i++){
+            for (int j=0;j<4;j++){
+                System.out.print(matriz1[i][j]);
+            }
+            System.out.println("");
+        }
+        //Mostramos matriz2
+        System.out.println("Matriz 2");
+        for (int i = 0;i<4;i++){
+            for (int j=0;j<4;j++){
+                System.out.print(matriz1[j][i]);
+            }
+            System.out.println("");
+        }
+    }
+}
