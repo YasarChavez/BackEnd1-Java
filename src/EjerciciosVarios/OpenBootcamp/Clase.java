db691bd91499a83f6a56a9d47644aa5af820f076
YasarChavez <tneniitoo@gmail.com>
2023-03-20 00:45:35 -0300

    Ejercicios Guia 4

--- src/EjerciciosVarios/OpenBootcamp/Clase.java
+++ src/EjerciciosVarios/OpenBootcamp/Clase.java
@@ -1,4 +1,4 @@
 package EjerciciosVarios.OpenBootcamp;
 
-public class Main {
+public class Clase {
 }
--- src/EjerciciosVarios/OpenBootcamp/IntroProgramacion/ClaseCoche.java
+++ src/EjerciciosVarios/OpenBootcamp/IntroProgramacion/ClaseCoche.java
@@ -1,6 +1,6 @@
 package EjerciciosVarios.OpenBootcamp.IntroProgramacion;
 
-public class Main {
+public class ClaseCoche {
     public static void main(String[] args) {
         /*Primera parte: Crear una función con tres parámetros que sean números que se suman entre sí.
         Llamar a la función en el main y darle valores.
