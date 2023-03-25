68f40a46d7ac169dd5fb6b6e01426af6ed72fa30
Yasar Chavez <tneniitoo@gmail.com>
2023-03-25 14:02:51 +0000

    nada

--- src/EjerciciosVarios/Varios/FizzBuzz.java
+++ src/EjerciciosVarios/Varios/FizzBuzz.java
@@ -1,10 +1,16 @@
 package EjerciciosVarios.Varios;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
         //Escribir "Fizz" cuando I sea multiplo de 3.
         //"Buzz" cuando sea multiplo de 5 y "FizzBuzz" cuando es multiplo de ambos.
-        for (int i = 1; i <= 100; i++) {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el maximo de numeros a generar:");
+        int num = leer.nextInt();
+        
+        for (int i = 1; i <= num; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
             } else if (i % 5 == 0) {
