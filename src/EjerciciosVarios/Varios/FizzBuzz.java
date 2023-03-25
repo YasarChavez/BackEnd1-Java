286aabb725ff312c48770dd9305d5481caf4dc48
YasarChavez <tneniitoo@gmail.com>
2023-03-25 12:13:31 -0300

    Acomodando archivos.

--- src/EjerciciosVarios/Varios/FizzBuzz.java
+++ src/EjerciciosVarios/Varios/FizzBuzz.java
@@ -9,7 +9,7 @@ public class FizzBuzz {
         Scanner leer = new Scanner(System.in);
         System.out.println("Ingrese el maximo de numeros a generar:");
         int num = leer.nextInt();
-        
+        //Mejoramos FizzBuzz
         for (int i = 1; i <= num; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("FizzBuzz");
