8fbffff8fdffb53d47ff66b127570945c35cb292
YasarChavez <tneniitoo@gmail.com>
2023-03-27 22:30:13 -0300

    Arg Programa Guia 7 Objetos

--- src/ArgentinaProgramaEGG/ArgProgramaJavaGuia07POO/Extras/Ejercicios/Ejerc02.java
+++ src/ArgentinaProgramaEGG/ArgProgramaJavaGuia07POO/Extras/Ejercicios/Ejerc02.java
@@ -5,9 +5,6 @@ import ArgentinaProgramaEGG.ArgProgramaJavaGuia07POO.Extras.entidades.Puntos;
 public class Ejerc02 {
     public static void main(String[] args) {
         Puntos punto1 = new Puntos().crearPuntos();
-        Puntos punto2 = new Puntos().crearPuntos();
-
-        System.out.println(punto1.toString());
-        System.out.println(punto2.toString());
+        System.out.println(Puntos.Distancia(punto1));
     }
 }
--- src/ArgentinaProgramaEGG/ArgProgramaJavaGuia07POO/Extras/entidades/Puntos.java
+++ src/ArgentinaProgramaEGG/ArgProgramaJavaGuia07POO/Extras/entidades/Puntos.java
@@ -13,45 +13,72 @@ existe entre los dos puntos que existen en la clase Puntos. Para conocer
 como calcular la distancia entre dos puntos consulte el siguiente link:
 * */
 public class Puntos {
-    private int pos1;
-    private int pos2;
+    private double pos1;
+    private double pos2;
+
+    private double pos3;
+    private double pos4;
 
     public Puntos() {
     }
 
-    public Puntos(int pos1, int pos2) {
+    public Puntos(double pos1, double pos2, double pos3, double pos4) {
         this.pos1 = pos1;
+        this.pos2 = pos2;
+        this.pos3 = pos3;
+        this.pos4 = pos4;
+    }
+
+    public void setPos1(double pos1) {
+        this.pos1 = pos1;
+    }
+
+    public void setPos2(double pos2) {
         this.pos2 = pos2;
     }
 
-    public void setPos1(int pos1) {
-        this.pos1 = pos1;
+    public void setPos3(double pos3) {
+        this.pos3 = pos3;
     }
 
-    public void setPos2(int pos2) {
-        this.pos2 = pos2;
+    public void setPos4(double pos4) {
+        this.pos4 = pos4;
     }
 
-    public int getPos1() {
+    public double getPos1() {
         return pos1;
     }
 
-    public int getPos2() {
+    public double getPos2() {
         return pos2;
+    }
+
+    public double getPos3() {
+        return pos3;
+    }
+
+    public double getPos4() {
+        return pos4;
     }
 
     public Puntos crearPuntos(){
         Scanner leer = new Scanner(System.in);
         Puntos punto = new Puntos();
         System.out.println("Ingrece Pos 1");
-        punto.setPos1(leer.nextInt());
+        punto.setPos1(leer.nextDouble());
         System.out.println("Ingrece Pos 2");
-        punto.setPos1(leer.nextInt());
+        punto.setPos1(leer.nextDouble());
+        System.out.println("Ingrece Pos 3");
+        punto.setPos1(leer.nextDouble());
+        System.out.println("Ingrece Pos 4");
+        punto.setPos4(leer.nextDouble());
         return punto;
     }
 
-    public double Distancia(int pos1, int pos2){
-        double distancia = Math.sqrt(Math.pow(getPos1() - getPos2(), 2) + Math.pow(y2 - y1, 2));
+    public static double Distancia(Puntos punto1){
+        double distancia;
+        //distancia = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return distancia =Math.sqrt(Math.pow(punto1.getPos2() - punto1.getPos1(), 2) + Math.pow(punto1.getPos4() - punto1.getPos3(), 2));
     }
 
 
