14d5bfa815673fe2eff1d1790319294ac3493331
YasarChavez <tneniitoo@gmail.com>
2023-04-01 13:07:48 -0300

    Arg Programa Guia 8 Objetos/Servicios

--- src/ArgentinaProgramaEGG/ArgProgramaJavaGuia08ClaseServicio/Ejerc03.java
+++ src/ArgentinaProgramaEGG/ArgProgramaJavaGuia08ClaseServicio/Ejerc03.java
@@ -7,7 +7,15 @@ import ArgentinaProgramaEGG.ArgProgramaJavaGuia08ClaseServicio.Servicio.PersonaS
 /*Crear 4 objetos de tipo Persona con distintos valores, a continuación,
 llamaremos todos los métodos para cada objeto, deberá comprobar si la
 persona está en su peso ideal, tiene sobrepeso o está por debajo de su
-peso ideal e indicar para cada objeto si la persona es mayor de edad.*/
+peso ideal e indicar para cada objeto si la persona es mayor de edad.
+
+Por último, guardaremos los resultados de los métodos calcularIMC y
+esMayorDeEdad en distintas variables(arrays), para después calcular un
+porcentaje de esas 4 personas cuantas están por debajo de su peso,
+cuantas en su peso ideal y cuantos, por encima, y también calcularemos
+un porcentaje de cuantos son mayores de edad y cuantos menores. Para
+esto, podemos crear unos métodos adicionales.
+*/
 
 public class Ejerc03 {
     public static void main(String[] args) {
