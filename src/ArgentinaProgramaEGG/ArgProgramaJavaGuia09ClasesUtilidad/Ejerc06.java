27cc83b107d8c37d67cd882b5ad3db70730908ed
YasarChavez <tneniitoo@gmail.com>
2023-04-15 01:11:14 -0300

    Ejercicios guía 9 POO/Utilidad.

---
+++ src/ArgentinaProgramaEGG/ArgProgramaJavaGuia09ClasesUtilidad/Ejerc06.java
@@ -0,0 +1,14 @@
+package ArgentinaProgramaEGG.ArgProgramaJavaGuia09ClasesUtilidad;
+
+import ArgentinaProgramaEGG.ArgProgramaJavaGuia09ClasesUtilidad.Entidades.Curso;
+import ArgentinaProgramaEGG.ArgProgramaJavaGuia09ClasesUtilidad.Servicios.CursoService;
+
+import java.util.Arrays;
+
+public class Ejerc06 {
+    public static void main(String[] args) {
+        CursoService service = new CursoService();
+        Curso grupo1=service.crearCurso();
+        service.resumenCurso(grupo1);
+    }
+}
